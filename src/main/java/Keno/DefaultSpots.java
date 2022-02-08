b2f70d23401956a45c09388c101f70e654f0a8c7
Rita Cruz <rcchcz@gmail.com>
2022-02-08 10:43:38 -0300

    Update DefaultSpots.java

--- src/main/java/Keno/DefaultSpots.java
+++ src/main/java/Keno/DefaultSpots.java
@@ -6,20 +6,19 @@ public class DefaultSpots implements GameMode{
     Scanner scan = new Scanner(System.in);
 
     public Set<Integer> setSpots() {
-        System.out.println("How many numbers do you want to bet? The limit is 15.");
-        int qtd = scan.nextInt();
-        if(qtd > 15) {
-            qtd = 15;
+        System.out.println(">>> How many numbers do you want to bet? The limit is 15.");
+        int betSize = scan.nextInt();
+        if(betSize > 15) {
+            betSize = 15;
         }
-        System.out.println("Insert the bet numbers separated by space. The proper range is [1, 80].");
+        System.out.println(">>> Insert the bet numbers separated by space. The proper range is [1, 80].");
         Set<Integer> spots = new TreeSet<>();
-        for(int i = 0; i < qtd; i++) {
+        for(int i = 0; i < betSize; i++) {
             int num = scan.nextInt();
             if(num >= 1 && num <= 80) {
                 spots.add(num);
             }
         }
-        System.out.println(spots);
         return spots;
     }
 }
